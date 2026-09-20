14e07e8ec5e51a64f3d1e7daa9d16ed348ca4c60
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-20 21:12:30 +0600

    Added:Loading-System

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className='text-center mt-[200px]'>
+            <span className="loading loading-spinner loading-xl"></span>
+        </div>
+    );
+};
+
+export default loading;
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -31,23 +31,6 @@ const Navbar = () => {
             tabIndex={-1}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
           </ul>
         </div>
         <div className="flex gap-3 items-center">
@@ -62,22 +45,6 @@ const Navbar = () => {
           </li>
           <li>
             <Link href="/listed-books">Listed Books</Link>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
           </li>
         </ul>
       </div>
